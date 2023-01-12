8824beb7e49838ddbc86630ef00af2b09cfb1956
Nora Hansen <nora.hans1@hotmail.com>
2023-01-12 14:48:43 +0100

    Added ability to change background color of javascript page (not persistent)

--- js/javaFunScript.js
+++ js/javaFunScript.js
@@ -2,6 +2,20 @@
 let pp = document.getElementById('ppTxt');
 console.log(pp);
 
+//Background color
+let bg = document.getElementById('mainBg');
+
+// Change peepee poopoo text
 function ppTextChange(button) {
     pp.textContent = button;
+}
+
+// Change background of main
+function changeBgColor(c) {
+    if (c == '') {
+        let customColor = document.getElementById('iColor').value;
+        c = customColor;
+    }
+    bg.style.backgroundColor = c;
+
 }
