b8ee41291af01cf5a1a29dc1a2b92fe25bafe718
Nora Hansen <nora.hans1@hotmail.com>
2023-01-12 18:28:52 +0100

    Random color bg

--- js/javaFunScript.js
+++ js/javaFunScript.js
@@ -1,21 +1,35 @@
-//Peepee or poopoo
-let pp = document.getElementById('ppTxt');
-console.log(pp);
-
-//Background color
-let bg = document.getElementById('mainBg');
+let pp = document.getElementById('ppTxt');  //Peepee or poopoo text
+let bg = document.getElementById('mainBg'); //Background color of main tag
 
 // Change peepee poopoo text
 function ppTextChange(button) {
     pp.textContent = button;
 }
 
-// Change background of main
+/**
+ * @brief   Change background color of main tag
+ * 
+ * @see -   genRandomColor()
+ */
 function changeBgColor(c) {
+    // If color name is empty, set to custom color
     if (c == '') {
         let customColor = document.getElementById('iColor').value;
         c = customColor;
+        // If color name is random, generate random color
+    } else if (c == 'random') {
+        c = genRandomColor();
     }
+    // Set color of background to new color
     bg.style.backgroundColor = c;
 
+}
+
+// Generate random color RGB
+function genRandomColor() {
+    let r =Math.floor(Math.random() * 256);
+    let g = Math.floor(Math.random() * 256);
+    let b = Math.floor(Math.random() * 256);
+    let color = 'rgb(' + r + ',' + g + ',' + b + ')';
+    return color;
 }
